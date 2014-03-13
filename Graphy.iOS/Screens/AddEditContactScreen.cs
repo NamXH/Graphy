147a8c4e7a797e85bcf9f0aa2f595296db178fd4
Nam Hoang <nam.xuan.hoang.work@gmail.com>
2014-03-13 00:10:40 -0600

    Add delete button

--- Graphy.iOS/Screens/AddEditContactScreen.cs
+++ Graphy.iOS/Screens/AddEditContactScreen.cs
@@ -11,12 +11,12 @@ namespace Graphy.iOS
     {
         public AddEditContactScreen() : base(UITableViewStyle.Grouped, null, true)
         {
+            // Navigation and root
             NavigationItem.LeftBarButtonItem = new UIBarButtonItem(UIBarButtonSystemItem.Cancel, (sender, args) =>
             {
                 NavigationController.PopViewControllerAnimated(false);
             });
             NavigationItem.RightBarButtonItem = new UIBarButtonItem(UIBarButtonSystemItem.Done, DoneButtonClicked);
-
             Root = new RootElement("");
 
             // Phone numbers
@@ -31,8 +31,37 @@ namespace Graphy.iOS
                 phoneSection.Insert(phoneSection.Count - 1, phoneType);
                 var number = new EntryElement(null, "Phone Number", null);
                 phoneSection.Insert(phoneSection.Count - 1, number);
+
+                var deleteButtonOn = false;
+                phoneType.AccessoryTapped += () =>
+                {
+                    if (!deleteButtonOn)
+                    {
+                        deleteButtonOn = true;
+                        var deleteButton = new StyledStringElement("Delete This Number")
+                        {
+                            BackgroundColor = UIColor.Red,
+                        };
+                        phoneSection.Insert(phoneType.IndexPath.Row + 2, deleteButton);
+                    }
+                };
             });
             phoneSection.Add(phoneLoadMore);
+
+//            phoneSection.Add(new StyledStringElement("abc")
+//            {
+////                BackgroundColor = UIColor.Green,
+//            });
+
+//            var btn = new StyledStringElement("Delete This Number")
+//            {
+//                BackgroundColor = UIColor.Red,
+//            };
+//            phoneSection.Insert(phoneSection.Count - 1, btn);
+
+            // Connections
+//            var connectionSection = new Section();
+//            Root.Add(connectionSection);
 
 //            Root.Add(new Section() { new StyledStringElement("abc") });
 //            TableView.SetEditing(true, true);
@@ -42,50 +71,48 @@ namespace Graphy.iOS
         {
 
         }
-
-//        public override Source CreateSizingSource(bool unevenRows)
-//        {
-//            //                if (unevenRows)
-//            //                    throw new NotImplementedException("You need to create a new SourceSizing subclass, this sample does not have it");
-//            return new EditingSource(this);
-//        }
+        //        public override Source CreateSizingSource(bool unevenRows)
+        //        {
+        //            //                if (unevenRows)
+        //            //                    throw new NotImplementedException("You need to create a new SourceSizing subclass, this sample does not have it");
+        //            return new EditingSource(this);
+        //        }
     }
-
-//    public class EditingSource : DialogViewController.Source
-//    {
-//        public EditingSource(DialogViewController dvc) : base(dvc)
-//        {
-//        }
-//
-//        public override bool CanEditRow(UITableView tableView, NSIndexPath indexPath)
-//        {
-//            // Trivial implementation: we let all rows be editable, regardless of section or row
-//            return true;
-//        }
-//
-//        public override UITableViewCellEditingStyle EditingStyleForRow(UITableView tableView, NSIndexPath indexPath)
-//        {
-//            var section = Container.Root[indexPath.Section];
-//            var element = section[indexPath.Row];
-//
-//            if (element is StyledStringElement)
-//            {
-//                return UITableViewCellEditingStyle.Delete;
-//            }
-//            else
-//            {
-//                return UITableViewCellEditingStyle.None;
-//            }
-//        }
-//
-//        public override void CommitEditingStyle(UITableView tableView, UITableViewCellEditingStyle editingStyle, NSIndexPath indexPath)
-//        {
-//            //
-//            // In this method, we need to actually carry out the request
-//            //
-//            var section = Container.Root[indexPath.Section];
-//            var element = section[indexPath.Row];
-//            section.Remove(element);
-//        }
-//    }
+    //    public class EditingSource : DialogViewController.Source
+    //    {
+    //        public EditingSource(DialogViewController dvc) : base(dvc)
+    //        {
+    //        }
+    //
+    //        public override bool CanEditRow(UITableView tableView, NSIndexPath indexPath)
+    //        {
+    //            // Trivial implementation: we let all rows be editable, regardless of section or row
+    //            return true;
+    //        }
+    //
+    //        public override UITableViewCellEditingStyle EditingStyleForRow(UITableView tableView, NSIndexPath indexPath)
+    //        {
+    //            var section = Container.Root[indexPath.Section];
+    //            var element = section[indexPath.Row];
+    //
+    //            if (element is StyledStringElement)
+    //            {
+    //                return UITableViewCellEditingStyle.Delete;
+    //            }
+    //            else
+    //            {
+    //                return UITableViewCellEditingStyle.None;
+    //            }
+    //        }
+    //
+    //        public override void CommitEditingStyle(UITableView tableView, UITableViewCellEditingStyle editingStyle, NSIndexPath indexPath)
+    //        {
+    //            //
+    //            // In this method, we need to actually carry out the request
+    //            //
+    //            var section = Container.Root[indexPath.Section];
+    //            var element = section[indexPath.Row];
+    //            section.Remove(element);
+    //        }
+    //    }
 }
